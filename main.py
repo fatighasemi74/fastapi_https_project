1bb49d0459e4cf3470c23511b765b2c4298d0425
fati <faateme.ghasemii@gmail.com>
2024-04-14 13:50:39 -0400

    templates for post

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import httpx
 
+
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 async def get_http_client():
     async with httpx.AsyncClient() as client:
@@ -12,15 +16,16 @@ async def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/posts")
-async def get_posts(client: httpx.AsyncClient = Depends(get_http_client)):
+@app.get("/posts", response_class=HTMLResponse)
+async def get_posts(request: Request, client: httpx.AsyncClient = Depends(get_http_client)):
     response = await client.get("https://jsonplaceholder.typicode.com/posts")
-    return response.json()
+    return templates.TemplateResponse("posts.html", {"request": request, "posts": response.json()})
 
 
-@app.get("/posts/{post_id}")
-async def get_post(post_id: int, client: httpx.AsyncClient = Depends(get_http_client)):
+@app.get("/posts/{post_id}", response_class=HTMLResponse)
+async def get_post(request: Request, post_id: int, client: httpx.AsyncClient = Depends(get_http_client)):
     response = await client.get(f"https://jsonplaceholder.typicode.com/posts/{post_id}")
     if response.status_code == 404:
         raise HTTPException(status_code=404, detail="Post not found")
-    return response.json()
+    post = response.json()
+    return templates.TemplateResponse("post.html", {"request": request, "post": post})
